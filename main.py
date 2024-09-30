93622d7a6fcadb18667b73dfb2d6627fa5d41410
Diego Costa <diegocosta.doc23@gmail.com>
2024-09-30 14:33:10 -0300

    mudando o nome do arquivo html de mypage para index para o github pages

---
+++ main.py
@@ -0,0 +1,22 @@
+# site com os scripts: https://cdnjs.com/ 
+# pip install python-socketio flask-socketio simple-websocket
+
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+# funcionalidade de enviar mensagem
+@socketio.on("message")
+def gerenciar_mensagem(mensagem):
+    send(mensagem, broadcast=True)
+
+
+# criar a minha 1ª página = 1ª rota
+@app.route("/") # decorator
+def mypage():
+    return render_template("mypage.html")
+
+# roda o meu aplicativo
+socketio.run(app, host="192.168.0.19")
